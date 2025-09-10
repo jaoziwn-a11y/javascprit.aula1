56152e0f24bba416649aff462348f4588817b5c2
João Victor Martins <jaoziwn@gmail.com>
2025-09-10 23:51:23 +0000

    adiciona estrutura condicional para aprovação e reprovação com logs

---
+++ condicionais.js
@@ -0,0 +1,17 @@
+/*
+
+se (nota >= 7.) {
+escreva ("aprovado!")
+}senao{
+escreva ("reprovado!")
+}
+*/
+
+let nota = 7.
+if (nota >= 7.) {
+    console.log("Aprovado!");
+}else{
+    console.log ("Reprovado!");
+}
+
+console.log (typeof nota);
