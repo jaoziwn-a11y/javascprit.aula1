b7f982e3d7618dda6649062ca68b45f1db83c89e
João Victor Martins <jaoziwn@gmail.com>
2025-09-10 23:43:11 +0000

    adiciona perfil pessoal com nome, idade, cidade e interesse em programação

---
+++ exercicio-pratico.js
@@ -0,0 +1,11 @@
+let meuNome = "João Martins";
+let minhaIdade = 17;
+let minhaCidade = "Campinas - SP";
+let gostaDeProgramar = true;
+
+
+console.log("=== MEU PERFIL ===");
+console.log(`Nome: ${meuNome}`);
+console.log(`Idade: ${minhaIdade} anos`);
+console.log(`Cidade: ${minhaCidade}`);
+console.log(`Gosta de Programar: ${gostaDeProgramar}`);
