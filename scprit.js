61bc0c8bd7d37a97a7ff4172f067234857271c8f
jaoziwn-a11y <jaoziwn@gmail.com>
2025-09-10 19:39:47 -0300

    alteração no javascprit

---
+++ scprit.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+let nome
+let sobrenome
+let idade
+let altura
+let ehHumano
+
+nome = "João"
+sobrenome = "Martins"
+idade = 17
+altura = 1.73
+ehHumano = true
+
+console.log("Nome: " + nome)
+console.log("Sobrenome: " + sobrenome)
+console.log("Idade: " + idade)
+console.log("Altura: " + altura)
+console.log("É humano? " + ehHumano)
