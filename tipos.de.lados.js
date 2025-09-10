d8e82c243bb3157a097e833bc3dabd83776eefd1
João Victor Martins <jaoziwn@gmail.com>
2025-09-10 23:30:24 +0000

    adiciona variáveis e logs de tipos em tipos.de.lados.js

---
+++ tipos.de.lados.js
@@ -0,0 +1,13 @@
+let texto = "Hello Word"
+let numero = 42;
+let decimal = 3.14;
+let verdadeiro = true;
+let nulo = null;
+let indefinido;
+
+console.log (typeof texto);
+console.log (typeof numero);
+console.log (typeof decimal);
+console.log (typeof verdadeiro);
+console.log (typeof nulo);
+console.log (typeof indefinido);
